0f54fa5a683794c865191c13035b45c2d94ccb4f
emilg1101 <emilyandro99@gmail.com>
2019-08-17 20:25:17 +0300

    init

---
+++ app/src/main/java/com/github/emilg1101/spacex/sdk/sample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.emilg1101.spacex.sdk.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
