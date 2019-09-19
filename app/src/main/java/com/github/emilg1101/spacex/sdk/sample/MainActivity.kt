532834b7daeca246f01ceae036f2876f14e8d75f
emilg1101 <emilyandro99@gmail.com>
2019-09-19 22:09:08 +0300

    update gradle

--- app/src/main/java/com/github/emilg1101/spacex/sdk/sample/MainActivity.kt
+++ app/src/main/java/com/github/emilg1101/spacex/sdk/sample/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.github.emilg1101.spacex.api.sdk.SpaceXCallback
 import com.github.emilg1101.spacex.api.sdk.entity.history.HistoricalEvent
-import com.github.emilg1101.spacex.api.sdk.request.AllHistoricalEventsRequest
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,8 +13,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val api = (application as App).getSpaceXApi()
-
-        AllHistoricalEventsRequest().execute()
 
         api.oneHistoricalEvent(0).execute(object : SpaceXCallback<HistoricalEvent> {
 
