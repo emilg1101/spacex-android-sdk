a0a3be4e99dda301fb703efa01ea93180da5327c
emilg1101 <emilyandro99@gmail.com>
2019-09-20 17:57:50 +0300

    api info test

--- spacexsdk/src/test/java/com/github/emilg1101/spacex/api/sdk/SpaceXTest.java
+++ spacexsdk/src/test/java/com/github/emilg1101/spacex/api/sdk/SpaceXTest.java
@@ -2,6 +2,7 @@ package com.github.emilg1101.spacex.api.sdk;
 
 import android.content.Context;
 
+import com.github.emilg1101.spacex.api.sdk.entity.info.ApiInfo;
 import com.github.emilg1101.spacex.api.sdk.entity.info.CompanyInfo;
 
 import org.junit.Assert;
@@ -23,12 +24,18 @@ public class SpaceXTest {
     @Before
     public void init() {
         MockitoAnnotations.initMocks(this);
+        api = new SpaceX.Builder(context).build();
     }
 
     @Test
     public void companyInfo() {
-        api = new SpaceX.Builder(context).build();
         CompanyInfo info = api.companyInfo().execute();
         Assert.assertEquals("SpaceX", info.getName());
+    }
+
+    @Test
+    public void apiInfo() {
+        ApiInfo info = api.apiInfo().execute();
+        Assert.assertEquals("SpaceX-API", info.getProjectName());
     }
 }
